d2063530f6a39c3af95c4a57769c3a79725b3db1
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-14 17:14:11 +0300

    Feat unimplemented routes

---
+++ controllers/petFoodInstanceController.js
@@ -0,0 +1,30 @@
+const PetFoodInstance = require("../models/petFoodInstance");
+const asyncHandler = require("express-async-handler");
+
+exports.pet_food_instance_list = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_create_get = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_create_post = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_update_get = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_update_post = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_delete_get = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});
+
+exports.pet_food_instance_delete_post = asyncHandler(async (req, res, next) => {
+	res.send("NOT IMPLEMENTED YET");
+});

7c43ddd02481bf9ae2f12b70564e8427e0e4379f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-16 16:27:54 +0300

    Fix route naming mistake

--- routes/animals.js
+++ routes/animals.js
@@ -7,77 +7,68 @@ const petFoodInstanceController = require("../controllers/petFoodInstanceControl
 
 // category routes
 
-router.get("/animals", petFoodCategoryController.pet_food_category_list);
+router.get("", petFoodCategoryController.pet_food_category_list);
+
+router.get("/create", petFoodCategoryController.pet_food_category_create_get);
+
+router.post("/create", petFoodCategoryController.pet_food_category_create_post);
 
 router.get(
-	"/animals/create",
-	petFoodCategoryController.pet_food_category_create_get
-);
-
-router.post(
-	"/animals/create",
-	petFoodCategoryController.pet_food_category_create_post
-);
-
-router.get(
-	"/animals/:id/update",
+	"/:id/update",
 	petFoodCategoryController.pet_food_category_update_get
 );
 
 router.post(
-	"/animals/:id/update",
+	"/:id/update",
 	petFoodCategoryController.pet_food_category_update_post
 );
 
 router.get(
-	"/animals/:id/delete",
+	"/:id/delete",
 	petFoodCategoryController.pet_food_category_delete_get
 );
 
 router.post(
-	"/animals/:id/delete",
+	"/:id/delete",
 	petFoodCategoryController.pet_food_category_delete_post
 );
 
 // instance routes
 
-router.get(
-	"/animals/:id/pet_foods",
-	petFoodInstanceController.pet_food_instance_list
-);
+router.get("/:id/pet_foods", petFoodInstanceController.pet_food_instance_list);
 
 router.get(
-	"/animals/:id/create",
-	petFoodInstanceController.pet_food_category_create_get
+	"/:id/create",
+	petFoodInstanceController.pet_food_instance_create_get
 );
 
 router.post(
-	"/animals/:id/create",
-	petFoodInstanceController.pet_food_category_create_post
+	"/:id/create",
+	petFoodInstanceController.pet_food_instance_create_post
 );
 
 router.get(
-	"/animals/:id/:instance_id",
+	"/:id/:instance_id",
 	petFoodInstanceController.pet_food_instance_details
 );
 
 router.get(
-	"/animals/:id/:instance_id/update",
+	"/:id/:instance_id/update",
 	petFoodInstanceController.pet_food_instance_update_get
 );
 
 router.post(
-	"/animals/:id/:instance_id/update",
+	"/:id/:instance_id/update",
 	petFoodInstanceController.pet_food_instance_update_post
 );
 
 router.get(
-	"/animals/:id/:instance_id/delete",
+	"/:id/:instance_id/delete",
 	petFoodInstanceController.pet_food_instance_delete_get
 );
 
 router.post(
-	"/animals/:id/:instance_id/delete",
+	"/:id/:instance_id/delete",
 	petFoodInstanceController.pet_food_instance_delete_post
 );
 

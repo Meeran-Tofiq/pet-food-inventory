15101fe17e590ac54187b6d3d80c7776d79aab17
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-17 13:16:31 +0300

    Feat route handling for animals list page

--- controllers/petFoodCategoryController.js
+++ controllers/petFoodCategoryController.js
@@ -2,7 +2,12 @@ const PetFoodCategory = require("../models/petFoodCategory");
 const asyncHandler = require("express-async-handler");
 
 exports.pet_food_category_list = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED YET");
+	const categoriesList = await PetFoodCategory.find({}).exec();
+
+	res.render("animals_list", {
+		title: "Animals",
+		categoriesList,
+	});
 });
 
 exports.pet_food_category_create_get = asyncHandler(async (req, res, next) => {

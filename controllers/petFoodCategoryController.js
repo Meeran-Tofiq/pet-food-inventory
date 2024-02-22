22f8e71ea287682f6f3622ead711324a23ea9ab3
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-22 18:44:07 +0300

    Feat route handling for creating a category

--- controllers/petFoodCategoryController.js
+++ controllers/petFoodCategoryController.js
@@ -1,4 +1,5 @@
 const PetFoodCategory = require("../models/petFoodCategory");
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.pet_food_category_list = asyncHandler(async (req, res, next) => {
@@ -11,12 +12,35 @@ exports.pet_food_category_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.pet_food_category_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED YET");
+	res.render("category_form", {
+		title: "Create Category",
+	});
 });
 
-exports.pet_food_category_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED YET");
-});
+exports.pet_food_category_create_post = [
+	body("animal_type", "Animal type must contain at least 3 characters.")
+		.trim()
+		.isLength(3)
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		const category = new petFoodCategory({
+			animal_type: req.body.animal_type,
+		});
+
+		if (!errors.isEmpty()) {
+			res.render("category_form", {
+				title: "Create Category",
+				category,
+				errors,
+			});
+			return;
+		}
+
+		await category.save();
+		res.redirect(category.url);
+	}),
+];
 
 exports.pet_food_category_update_get = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED YET");

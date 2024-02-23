4b5afbcc3aceaeff38fbdd439daab32d9bff3860
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-23 15:27:52 +0300

    Feat handling deleting a category POST

--- controllers/petFoodCategoryController.js
+++ controllers/petFoodCategoryController.js
@@ -102,5 +102,25 @@ exports.pet_food_category_delete_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.pet_food_category_delete_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED YET");
+	const [category, petFoodList] = await Promise.all([
+		PetFoodCategory.findById(req.params.category_id).exec(),
+		PetFoodInstance.find({ animal_type: req.params.category_id }).exec(),
+	]);
+
+	if (!category) {
+		res.redirect("/animals");
+		return;
+	}
+
+	if (petFoodList.length) {
+		res.render("category_delete", {
+			title: "Delete Category",
+			category,
+			petFoodList,
+		});
+		return;
+	}
+
+	await PetFoodCategory.findByIdAndDelete(req.body.category_id);
+	res.redirect("/animals");
 });

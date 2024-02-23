7219a7edeaf3810675a3f105a9ce1e43a826ed6a
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-23 15:07:25 +0300

    Feat handling for updating pet food

--- controllers/petFoodInstanceController.js
+++ controllers/petFoodInstanceController.js
@@ -97,9 +97,53 @@ exports.pet_food_instance_update_get = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.pet_food_instance_update_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED YET");
-});
+exports.pet_food_instance_update_post = [
+	body("name", "Name must have at least 3 characters")
+		.trim()
+		.isLength(3)
+		.escape(),
+	body("animal_type", "Animal must not be empty")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("in_stock", "Stock must be a valid number")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("price", "Price must not be empty")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		const petFood = new PetFoodInstance({
+			name: req.body.name,
+			animal_type: req.body.animal_type,
+			in_stock: req.body.in_stock,
+			price: req.body.price,
+			_id: req.params.instance_id,
+		});
+
+		if (!errors.isEmpty()) {
+			const animalsList = await petFoodCategory.find({}).exec();
+
+			res.render("pet_food_form", {
+				title: "Update Pet Food",
+				animalsList,
+				petFood,
+				errors: errors.array(),
+			});
+			return;
+		}
+
+		await PetFoodInstance.findByIdAndUpdate(
+			req.params.instance_id,
+			petFood,
+			{}
+		);
+		res.redirect(petFood.url);
+	}),
+];
 
 exports.pet_food_instance_delete_get = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED YET");

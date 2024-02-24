c75167aebb2150edbdfd00ef42d5fdb2900a9735
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-24 15:36:34 +0300

    Feat handling creating a new categ with user img

--- controllers/petFoodCategoryController.js
+++ controllers/petFoodCategoryController.js
@@ -3,6 +3,16 @@ const PetFoodInstance = require("../models/petFoodInstance");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+const multer = require("multer");
+const storage = multer.diskStorage({
+	destination: "public/images/animals",
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, file.fieldname + "-" + uniqueSuffix + ".jpg");
+	},
+});
+const upload = multer({ storage });
+
 exports.pet_food_category_list = asyncHandler(async (req, res, next) => {
 	const categoriesList = await PetFoodCategory.find({}).exec();
 
@@ -19,6 +29,7 @@ exports.pet_food_category_create_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.pet_food_category_create_post = [
+	upload.single("avatar"),
 	body("animal_type", "Animal type must contain at least 3 characters.")
 		.trim()
 		.isLength({ min: 3 })
@@ -27,6 +38,7 @@ exports.pet_food_category_create_post = [
 		const errors = validationResult(req);
 		const category = new PetFoodCategory({
 			animal_type: req.body.animal_type,
+			imageUrl: req.file.path.slice(7),
 		});
 
 		if (!errors.isEmpty()) {
